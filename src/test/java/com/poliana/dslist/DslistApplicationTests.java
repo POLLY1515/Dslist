fec89850dd9c89a0653d3639feb307073ed4db2b
POLLY1515 <amarantepolly2030@gmail.com>
2025-08-18 18:51:24 -0300

    Project created

---
+++ src/test/java/com/poliana/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poliana.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
